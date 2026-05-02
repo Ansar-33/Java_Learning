7eba6279c00d475d991d3e4ba12ab466414815cd
Ansar-33 <mohammedansar2k05@gmail.com>
2026-05-02 11:56:18 +0530

    Day6Programs

---
+++ Day6/Arraysinjava.java
@@ -0,0 +1,35 @@
+                               // Arrays Cloning //
+// import java.util.Arrays;
+// public class Arraysinjava {
+//     public static void main(String[] args){
+//         int arr[] = {1, 2, 3, 4, 5, 6};
+//         int b[] = arr.clone();                     // source and destination array should be of same type and size
+//         for(int i=0;i<b.length;i++){
+//             System.out.print(b[i]+" ");
+//         }
+//     }
+// }
+
+                           // import java.util.Arrays;
+// public class Arraysinjava {
+//     public static void main(String[] args){
+//         int arr[]  = {1,2,3,4,5,6};
+//         int b[] = Arrays.copyOf(arr, 6);     // source and destination array
+//         for(int i=0;i<b.length;i++){
+//             System.out.print(b[i]+" ");
+//         }
+//     }
+// }
+
+
+                          // System.arraycopy() method //
+public class Arraysinjava {
+    public static void main(String[] args){
+        int arr[]  = {1,2,3,4,5,6};
+        int b[] = new int[6];
+        System.arraycopy(arr, 2, b, 2, 4); 
+        for(int i=0;i<b.length;i++){
+            System.out.print(b[i]+" ");
+        }
+    }
+}
---
+++ Day6/arraylist.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class arraylist {
+    public static void main(String [] args){
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<String> str = new ArrayList<>();
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        System.out.println(arr);
+        arr.set(1,7);
+        System.out.println(arr);
+        arr.remove(2);
+        System.out.println(arr);
+        str.add("Mohammed");
+        str.add("Ansar");
+        str.add("S");
+        System.out.println(str);
+        str.remove(0);
+        System.out.println(str);
+        System.out.println(arr.contains(5));
+        System.out.println(str.contains("Ansar"));
+        arr.sort(null);
+        System.out.println(arr);
+        str.sort(null);
+        System.out.println(str);
+    }
+}
