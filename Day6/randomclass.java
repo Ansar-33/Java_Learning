7b8d270bc061f83ea74edba93d0e0e1caa55e3ae
Ansar-33 <mohammedansar2k05@gmail.com>
2026-05-02 19:10:10 +0530

    Day 1 Programs

--- Day6/randomclass.java
+++ Day6/randomclass.java
@@ -1,16 +1,26 @@
+import java.util.Arrays;
 import java.util.Random;
-
 public class randomclass {
     public static void main(String[] args) {
         Random rn =  new Random();
+
         int n = rn.nextInt(10);
         System.out.println(n);
+
         boolean f = rn.nextBoolean();
         System.out.println(f);
+
         double d = rn.nextDouble();
         System.out.println(d);
+
         double g = 10+(2*rn.nextGaussian());
         System.out.println(g);
+
+        byte[] b = new byte[5];
+        System.out.println(Arrays.toString(b));
+        
+        rn.nextBytes(b);
+        System.out.println(Arrays.toString(b));
         
     }
 }
