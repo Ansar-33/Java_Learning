f5d4349ee82f0b2d82e8ef1f9781e8d89d09618c
Ansar-33 <mohammedansar2k05@gmail.com>
2026-05-12 09:22:06 +0530

    Day9Programs

--- Day9/Ball.java
+++ Day9/Ball.java
@@ -1,9 +1,14 @@
+import java.util.Random;
+
 class Ball {
-boolean usable;
-Ball ball;
-public static void main (String[] args){
-Ball b = new Ball();
-System.out.print(b.usable);
-System.out.print(" "+ b.ball.usable);
-}
+
+    public static void main(String[] args) {
+        Random r1 = new Random(10);
+        Random r2 = new Random(10);
+        if (r1.nextInt() == r2.nextInt()) {
+            System.out.println("Jack");
+        } else {
+            System.out.println("Queen");
+        }
+    }
 }
---
+++ Day9/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+       int[] arr = {1, 2, 3, 4, 5}; 
+    
+for(int idx = 0; idx < arr.length; idx+=2) {
+        System.out.print(arr[idx]);
+    }
+}
+}
