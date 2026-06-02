b98118f4ab948f6688ae781338e903eb9256ffa9
Ansar-33 <mohammedansar2k05@gmail.com>
2026-06-02 12:12:50 +0530

    Day11Programs

---
+++ Day11/season.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class season {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Month No:");
+        int mn = sc.nextInt();
+        switch(mn){
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("Invalid Month");
+        }
+    }
+}
